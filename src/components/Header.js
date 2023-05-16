5a9ec6b77987f2cd7d1558fd5843430eb95dbfe0
robin-everaert <r-everaert@outlook.fr>
2023-05-16 19:06:05 +0200

    deploy to gh-pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ import NavBar from "./NavBar";
 const Header = () => {
     return (
         <header id='header'>
+            <div className="container-logo-header d-flex justify-content-center align-items-center p-3">
+                <img src="./img/logo.png" alt="Le logo de TMT Multitravaux" />
+            </div>
+
             <NavBar />
         </header>
     );
